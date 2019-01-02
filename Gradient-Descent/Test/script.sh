6b45be012f2c52f189aed17d611bc57a8f873d0b
mounikaponugoti <mounikaponaganti@gmail.com>
2019-01-02 10:22:56 -0600

    Update script

--- Gradient-Descent/Test/script.sh
+++ Gradient-Descent/Test/script.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
 
-workingPath=$PWD
+scriptPath=$(dirname "$(realpath $0)")
 Target="batchGradientDescent"
-exePath=${workingPath/"Test"/"bin"/}
+exePath=${scriptPath/"Test"/"bin"}
 
-#echo $exePath/$Target
+echo ""
+echo "Running batch gradient descent...."
+echo "Output files are in $scriptPath"
+echo ""
 
+sleep 1
+echo "$exePath/$Target -i input_1.txt -v 1 "
 $exePath/$Target -i input_1.txt -v 1 
-sleep 2
+
+slpeep 1
+echo "$exePath/$Target --input input_1.txt -v 1 -l 0.0001"
 $exePath/$Target --input input_1.txt -v 1 -l 0.0001
-sleep 2
+
+sleep 1
+echo "$exePath/$Target --input input_2.txt --variables 2 --iterations 500000 --epsilon 0.00001"
 $exePath/$Target --input input_2.txt --variables 2 --iterations 500000 --epsilon 0.00001
-sleep 2
+
+sleep 1
+echo "$exePath/$Target --input input_2.txt --variables 2 --learningrate 0.0001"
 $exePath/$Target --input input_2.txt --variables 2 --learningrate 0.0001
+
+sleep 1
+echo "$exePath/$Target --input input_2.txt --variables 2 --learningrate 0.0005 --iterations 5000000 --normalize true --whichnorm mean"
+$exePath/$Target --input input_2.txt --variables 2 --learningrate 0.0005 --iterations 5000000 --normalize true --whichnorm mean
+
+sleep 1
+echo "$exePath/$Target --input input_2.txt --variables 2 --learningrate 0.0005 --iterations 5000000 --normalize true --whichnorm minmax"
+$exePath/$Target --input input_2.txt --variables 2 --learningrate 0.0005 --iterations 5000000 --normalize true --whichnorm minmax
+echo ""
+echo "Done with running batch gradient descent...."
