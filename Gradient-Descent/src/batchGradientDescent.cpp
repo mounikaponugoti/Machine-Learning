eb35d1095878eb520e9c32376302b7741fd0d641
mounikaponugoti <mounikaponaganti@gmail.com>
2019-01-02 10:22:18 -0600

    Add normalization of inputs

--- Gradient-Descent/src/batchGradientDescent.cpp
+++ Gradient-Descent/src/batchGradientDescent.cpp
@@ -23,6 +23,8 @@ double epsilon = 0.0005;
 long int iterations = 100000;
 bool randomInitialParameterValues = true;
 bool userOutputFileName = false;
+bool normalize = false;
+std::string whichNorm = "mean";
 
 std::string help_msg = "-h or --help\n"
 "  To print this message\n"
@@ -32,6 +34,13 @@ std::string help_msg = "-h or --help\n"
 "  To specify output file name, default - gradientDecent_Output_[time].txt\n"
 "-r [true/false] or --random [true/false] \n"
 "  To specify whether to initialize the variables to random values or 0, default - true\n"
+"-n [true/false] or --normalize [true/false] \n"
+"  To specify whether to normalize the inputs or not, default - false\n"
+"-w [minmax/mean/standard] or --whichnorm [minmax/mean/standard]\n"
+"  Specify the type of normalization for the input samples, default - mean\n"
+"  minmax: min-max normalization x'= (x-min(x))/(max(x)-min(x))\n"
+"  mean: mean normalization x'= (x-mean(x))/(max(x)-min(x))\n"
+"  standard: standardization x'= (x-mean(x))/standardDeviation\n"
 "-v [numberOfVariables] or --variables [numberOfVariables]\n"
 "  To specify number of variables in a liner equation, default - 1\n"
 "-l [learningRate] or --learningrate [learningRate]\n"
@@ -50,73 +59,117 @@ public:
 	T actualOutput;
 };
 
+template <class T>
+class compare {
+public:
+	compare(int col) : m_column(col) {}
+	bool operator()(const sample<T>  i, const sample<T>  j) {
+		return i.input.at(m_column) < j.input.at(m_column);
+	}
+private:
+	int m_column;
+};
+
+
 void readCommandLine(int argc, char *argv[]) {
+	bool isUserSetWhichNorm = false;
 	for (int i = 1; i < argc; i++) {
 		std::string arg = argv[i];
 		if (strcmp(argv[i], "-i") == 0 || strcmp(argv[i], "--input") == 0) {
 			inputFileName = argv[i + 1];
 			i++;
-		}
-		else if (strcmp(argv[i], "-o") == 0 || strcmp(argv[i], "--output") == 0) {
+		} else if (strcmp(argv[i], "-o") == 0 || strcmp(argv[i], "--output") == 0) {
 			outputFileName = argv[i + 1];
 			userOutputFileName = true;
 			i++;
-		}
-		else if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--variables") == 0) {
+		} else if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--variables") == 0) {
 			std::istringstream ss(argv[i + 1]);
 			ss >> numberOfVariables;
 			i++;
-		}
-		else if (strcmp(argv[i], "-l") == 0 || strcmp(argv[i], "--learningRate") == 0) {
+		} else if (strcmp(argv[i], "-l") == 0 || strcmp(argv[i], "--learningrate") == 0) {
 			std::istringstream ss(argv[i + 1]);
 			ss >> learningRate;
 			i++;
-		}
-		else if (strcmp(argv[i], "-p") == 0 || strcmp(argv[i], "--epsilon") == 0) {
+		} else if (strcmp(argv[i], "-p") == 0 || strcmp(argv[i], "--epsilon") == 0) {
 			std::istringstream ss(argv[i + 1]);
 			ss >> epsilon;
 			i++;
-		}
-		else if (strcmp(argv[i], "-r") == 0 || strcmp(argv[i], "--random") == 0) {
+		} else if (strcmp(argv[i], "-r") == 0 || strcmp(argv[i], "--random") == 0) {
 			randomInitialParameterValues = (strcmp(argv[i + 1], "true") == 0 || strcmp(argv[i + 1], "True") == 0) ? true : false;
 			i++;
-		}
-		else if (strcmp(argv[i], "-k") == 0 || strcmp(argv[i], "--iterations") == 0) {
+		} else if (strcmp(argv[i], "-n") == 0 || strcmp(argv[i], "--normalize") == 0) {
+			normalize = (strcmp(argv[i + 1], "true") == 0 || strcmp(argv[i + 1], "True") == 0) ? true : false;
+			i++;
+		} else if (strcmp(argv[i], "-w") == 0 || strcmp(argv[i], "--whichnorm") == 0) {
+			whichNorm = argv[i + 1];
+			i++;
+			isUserSetWhichNorm = true;
+		} else if (strcmp(argv[i], "-k") == 0 || strcmp(argv[i], "--iterations") == 0) {
 			std::istringstream ss(argv[i + 1]);
 			ss >> iterations;
 			i++;
-		}
-		else if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
+		} else if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
 			std::cout << help_msg << std::endl;
 			exit(1);
-		}
-		else {
-			std::cout << "Error: Unknown parameter" << std::endl;
-			std::cout << help_msg << std::endl;
+		} else {
+			std::cerr << "Error: Unknown parameter " << argv[i] << std::endl;
+			std::cerr << help_msg << std::endl;
 			exit(1);
 		}
+	}
+	if (isUserSetWhichNorm &&(!normalize)) {
+		std::cerr << "Error: Turn on normalization to specify type of normalization " << std::endl;
+		std::cerr << help_msg << std::endl;
+		exit(1);
 	}
 }
 
 template <class T = float>
-void readInputFile(std::vector<sample<T> >& inputSampleData, std::ifstream& source) {
+void readInputFile(std::vector<sample<T> >& inputSampleData, std::ifstream& source, std::ofstream& destination) {
+	sample<T> tempSample;
 	std::string currentLine;
 	T readValue;
+	// Used to normalize the input samples
+	sample<T> s_min;
+	sample<T> s_max;
+	sample<double> s_mean;
+	sample<double> s_standDev;
+	if (normalize) {
+		// Resize the variables depending on user input
+		s_max.input.resize(numberOfVariables);
+		s_min.input.resize(numberOfVariables);
+		s_mean.input.resize(numberOfVariables);
+		s_standDev.input.resize(numberOfVariables);
+		// Initialize variables
+		for (int i = 0; i < numberOfVariables; i++) {
+			s_max.input[i] = std::numeric_limits<T>::min();
+			s_min.input[i] = std::numeric_limits<T>::max();
+			s_mean.input[i] = 0;
+			s_standDev.input[i] = 0;
+		}
+	}
+
+	tempSample.input.resize(numberOfVariables);
 	getline(source, currentLine);
 	while (source.good()) {
-#ifdef DEBUG
-		std::cout << currentLine << '\n';
-#endif
 		int i = 0;
-		sample<T> tempSample;
 		// Process nonempty lines
 		if (currentLine.length() != 0) {
 			std::istringstream ss(currentLine);
-			tempSample.input.reserve(numberOfVariables);
 			// Extract data from the current line
 			while (i < numberOfVariables) {
 				ss >> readValue;
-				tempSample.input.push_back(readValue);
+				tempSample.input[i] = readValue;
+				// Get the required data to normalize
+				if (normalize) {
+					if (readValue > s_max.input[i]) {
+						s_max.input[i] = readValue;
+					} 
+					if (readValue < s_min.input[i]) {
+						s_min.input[i] = readValue;
+					}
+					s_mean.input[i] += readValue;
+				}
 				// If the delimiter is ',' or '|' ignore it
 				if (ss.peek() == ',' || ss.peek() == '|') {
 					ss.ignore();
@@ -130,6 +183,56 @@ void readInputFile(std::vector<sample<T> >& inputSampleData, std::ifstream& sour
 		inputSampleData.push_back(tempSample);
 		getline(source, currentLine);
 	}
+
+	if (normalize) {
+		if (whichNorm == "minmax") {  // Min - max normalization
+			for (int i = 0; i < inputSampleData.size(); i++) {
+				for (int j = 0; j < numberOfVariables; j++) {
+					inputSampleData[i].input[j] = (inputSampleData[i].input[j] - s_min.input[j]) / (s_max.input[j] - s_min.input[j]);
+				}
+			}
+		} else if (whichNorm == "mean") {  // Mean normalization
+			// Calculate the mean
+			for (int i = 0; i < numberOfVariables; i++) {
+				s_mean.input[i] = s_mean.input[i] / inputSampleData.size();
+			}
+			for (int i = 0; i < inputSampleData.size(); i++) {
+				for (int j = 0; j < numberOfVariables; j++) {
+					inputSampleData[i].input[j] = (inputSampleData[i].input[j] - s_mean.input[j]) / (s_max.input[j] - s_min.input[j]);
+				}
+			}
+		} else if( whichNorm == "standard") { // Standardization
+			// Calculate the mean 
+			for (int i = 0; i < numberOfVariables; i++) {
+				s_mean.input[i] = s_mean.input[i] / inputSampleData.size();
+			}
+			// Calculate sum of square of errors
+			for (int i = 0; i < inputSampleData.size(); i++) {
+				for (int j = 0; j < numberOfVariables; j++) {
+					s_standDev.input[j] += ((inputSampleData[i].input[j] - s_mean.input[j])*(inputSampleData[i].input[j] - s_mean.input[j]));
+				}
+			}
+			// Calculate standard deviation for each variable
+			for (int j = 0; j < numberOfVariables; j++) {
+				s_standDev.input[j] += sqrt(s_standDev.input[j] / (inputSampleData.size() - 1));
+			}
+			// Standardize the input samples
+			for (int i = 0; i < inputSampleData.size(); i++) {
+				for (int j = 0; j < numberOfVariables; j++) {
+					inputSampleData[i].input[j] = (inputSampleData[i].input[j] - s_mean.input[j]) / s_standDev.input[j];
+				}
+			}
+		}
+
+		destination << "Normalized inputs " << std::endl;
+		for (int i = 0; i < inputSampleData.size(); i++) {
+			for (int j = 0; j < numberOfVariables; j++) {
+				destination << std::setprecision(4) << inputSampleData[i].input[j] << ", ";
+			}
+			destination << inputSampleData[i].actualOutput << std::endl;
+		}
+		destination << std::endl;
+	}
 }
 
 void initializeParameters(double* theta) {
@@ -138,8 +241,7 @@ void initializeParameters(double* theta) {
 		for (int i = 0; i < numberOfVariables + 1; i++) {
 			theta[i] = ((double)rand() / (RAND_MAX)) + 1;
 		}
-	}
-	else {
+	} else {
 		for (int i = 0; i < numberOfVariables + 1; i++) {
 			theta[i] = 0;
 		}
@@ -159,12 +261,11 @@ double calculateOutput(std::vector<sample<T> >& inputSampleData, double* theta, 
 // Print actual output and calculated output
 template <class T = float>
 void verify(std::vector<sample<T> >& inputSampleData, std::ofstream& destination, double* theta) {
-	destination << "\nActual Output, Calculated Output" << '\n';
+	destination << "\nActual Output, Calculated Output" << std::endl;
 	for (int i = 0; i < inputSampleData.size(); i++) {
 		double cal_output = calculateOutput<T>(inputSampleData, theta, i);
-		destination << std::setprecision(4) << inputSampleData[i].actualOutput << ", " << cal_output << '\n';
+		destination << std::setprecision(4) << inputSampleData[i].actualOutput << ", " << cal_output << std::endl;
 	}
-	destination << '\n';
 }
 
 // Calculate cost = ((actual output - calculated output)^2) / 2*m
@@ -212,7 +313,7 @@ void gradientDescent(std::vector<sample<T> >& inputSampleData, std::ofstream& de
 	for (int j = 0; j < numberOfVariables + 1; j++) {
 		destination << std::setprecision(6) << theta[j] << ", ";
 	}
-	destination << std::setprecision(6) << new_cost << '\n';
+	destination << std::setprecision(6) << new_cost << std::endl;
 #endif // DEBUG
 
 	while ((i < iterartions) && (fabs(new_cost - previous_cost) > epsilon)) {
@@ -231,7 +332,7 @@ void gradientDescent(std::vector<sample<T> >& inputSampleData, std::ofstream& de
 		for (int j = 0; j < numberOfVariables + 1; j++) {
 			destination << std::setprecision(6) << theta[j] << ", ";
 		}
-		destination << std::setprecision(6) << new_cost << '\n';
+		destination << std::setprecision(6) << new_cost << std::endl;
 #endif // DEBUG
 
 		if (min_cost > new_cost) {
@@ -241,11 +342,12 @@ void gradientDescent(std::vector<sample<T> >& inputSampleData, std::ofstream& de
 		i++;
 	}
 	// Log the final output
-	destination << "\n\nMinimum cost: " << min_cost << '\n';
+	destination << "\nMinimum cost: " << min_cost << std::endl;
 	destination << "\nLinear equation: ";
-	destination << "Y =  " << std::fixed << std::setprecision(6) << bestTheta[0];
+	destination << "Y =  " << std::fixed << std::setprecision(6) << bestTheta[0] << " ";
 	for (int j = 1; j < numberOfVariables + 1; j++) {
-		destination << " + " << std::setprecision(6) << bestTheta[j] << " x" << (j - 1);
+		//destination << (bestTheta[j] > 0 ? " + " : " - ");
+		destination << std::setprecision(6) << std::showpos << bestTheta[j] << " x" << std::noshowpos << (j - 1) << " ";
 	}
 	destination << std::endl;
 
@@ -263,7 +365,7 @@ int main(int argc, char *argv[]) {
 	std::vector<sample<double> > inputSampleData;
 	srand(time(NULL));
 	if (argc == 1) {
-		std::cerr << "Use ./gradientDescent --help for more options" << '\n';
+		std::cerr << "Use ./gradientDescent --help for more options" << std::endl;
 		return -1;
 	}
 	// Read command line arguments
@@ -282,17 +384,17 @@ int main(int argc, char *argv[]) {
 	// Open input file
 	source.open(inputFileName.c_str());
 	if (!source) {
-		std::cerr << "Error: Unable to open the input file" << '\n';
+		std::cerr << "Error: Unable to open the input file" << std::endl;
 		return -1;
 	}
 	// Open output file
 	destination.open(outputFileName.c_str());
 	if (!destination) {
-		std::cerr << "Error: Unable to open the output file" << '\n';
+		std::cerr << "Error: Unable to open the output file" << std::endl;
 		return -1;
 	}
 	// Read input data
-	readInputFile<double>(inputSampleData, source);
+	readInputFile<double>(inputSampleData, source, destination);
 	source.close();
 	initializeParameters(theta);
 
@@ -301,6 +403,10 @@ int main(int argc, char *argv[]) {
 	destination << "Number Of Variables: " << numberOfVariables << std::endl;
 	destination << "Number Of Max Iterations: " << iterations << std::endl;
 	destination << "Epsilon: " << epsilon << std::endl;
+	destination << "Normalize: " << std::boolalpha << normalize << std::noboolalpha << std::endl;
+	if (normalize) {
+		destination << "Which Normalization: " << whichNorm << std::endl;
+	}
 	destination << "Initial Theta Values: ";
 	for (int i = 0; i < numberOfVariables + 1; i++) {
 		destination << std::setprecision(6) << theta[i] << ", ";
